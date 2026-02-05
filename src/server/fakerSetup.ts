14cf62077e05bf4976b74534ce0c1b3bfc2e1546
Assylzhan Sarinov (ahmedok <a_sarinov@mail.ru>
2026-02-05 19:27:04 +0500

    feat: Add locale-aware probable "Single" instead of album name

--- src/server/fakerSetup.ts
+++ src/server/fakerSetup.ts
@@ -8,10 +8,19 @@ const {
     album: ruAlbum,
 } = russianMusicData;
 
+const albumProbability = 0.7;
+
 fakerRU.music.songName = () => fakerRU.helpers.arrayElement(ruSongName);
 fakerRU.music.artist = () => fakerRU.helpers.arrayElement(ruArtist);
 fakerRU.music.genre = () => fakerRU.helpers.arrayElement(ruGenre);
-fakerRU.music.album = () => fakerRU.helpers.arrayElement(ruAlbum);
+fakerRU.music.album = () =>
+    fakerRU.helpers.maybe(() => fakerRU.helpers.arrayElement(ruAlbum), {
+        probability: albumProbability,
+    }) ?? 'Сингл';
+
+const enOriginalAlbum = fakerEN.music.album.bind(fakerEN);
+fakerEN.music.album = () =>
+    fakerEN.helpers.maybe(() => enOriginalAlbum(), { probability: albumProbability }) ?? 'Single';
 
 export const FAKER_LOCALES: Record<string, Faker> = {
     en_US: fakerEN,
